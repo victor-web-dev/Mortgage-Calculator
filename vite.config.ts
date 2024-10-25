ee4cd3e60b7872ea18955259fe5c89302ca0d77b
Victor Araujo <victor_emanuel9@hotmail.com>
2024-10-24 20:50:04 -0700

    initial build

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tsConfigPaths()],
+  base: "./",
+  build: {
+    outDir: "build",
+  },
+  server: {
+    port: 3000,
+  },
+});
